b458061307b031eebab7f3416746d78f7a12af95
jilcimar <jilcimar.fernandes0267@gmail.com>
2024-01-22 09:31:46 -0300

    feat :closed_lock_with_key: configurando .env

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/route"
 	"fmt"
 	"log"
@@ -8,9 +9,12 @@ import (
 )
 
 func main() {
+	config.LoadEnv()
+
+	fmt.Println(config.StringConexction)
 	fmt.Println("Rodando API")
 
 	r := route.Generate()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
